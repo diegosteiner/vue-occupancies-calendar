5d3e8c2622411208f8d4994a9ec0e776ad1cbee1
Diego Steiner <diego.steiner@u041.ch>
2018-03-23 12:54:44 +0100

    Improve imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Calendar from './components/Calendar';
+import CalendarMonth from './components/CalendarMonth';
+import CalendarDay from './components/CalendarDay';
 
 export default {
     install(Vue) {
         Vue.component('calendar', Calendar);
+        Vue.component('calendar-month', CalendarMonth);
+        Vue.component('calendar-day', CalendarDay);
     },
 };
 
-export { Calendar };
+export { Calendar, CalendarMonth, CalendarDay };
