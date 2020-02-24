8a5e8ece41b4b81a11c1ed1134474e61deb1c4e4
Diego Steiner <diego.steiner@u041.ch>
2020-02-24 17:22:29 +0100

    Fix

--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -8,7 +8,7 @@
         v-for="day in daysOfMonth"
         class="calendar-day"
         :date="day.toISOString()"
-        :key="`day-${day.toISOString()}-${loading}`"
+        :key="`day-${day.toISOString()}`"
       >
         <slot :date="day"></slot>
       </time>
